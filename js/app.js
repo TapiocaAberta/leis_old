37fcfefd8924d245d142a3a95dca9e09f3b394c5
Paulo Luan <pauloluan.inova@gmail.com>
2014-10-20 14:57:10 -0200

    added markdown filter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-var myApp = angular.module('myApp', [
+var politiciansApp = angular.module('myApp', [
       'ngRoute',
       'politicianControllers',
       'lawControllers'
 ]);
+
+politiciansApp.filter('markdown', function ($sce) {
+    var converter = new Showdown.converter();
+    return function (value) {
+		var html = converter.makeHtml(value || '');
+        return $sce.trustAsHtml(html);
+    };
+});
