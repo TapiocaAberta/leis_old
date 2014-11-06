acdd5403886f4047899bfb66ad8ae0b28310f03a
Paulo Luan <pauloluan.inova@gmail.com>
2014-11-06 13:17:11 -0200

    changed images order

--- js/CarouselController.js
+++ js/CarouselController.js
@@ -5,17 +5,17 @@ carouselController.controller('CarouselDemoCtrl', ['$scope', function ($scope) {
             {
                   title: "Monitor Legislativo",
                   description: "Acompanhe de perto todos os projetos de lei criados pelos vereadores de São José dos Campos",
-                  image: "img/sjc1.jpg"
+                  image: "img/sjc3.jpg"
             },
             {
                   title: "Dados Abertos e Inovação",
                   description: "Conheça nosso movimento e descubra o porque um grupo de nerds resolveram unir tecnologia da informação com dados governamentais.",
-                  image: "img/sjc2.jpg",
+                  image: "img/sjc1.jpg",
             },
             {
                   title: "Precisamos de Ajuda!",
                   description: "Queremos ajudar a prefeitura a renovar a forma de se fazer política, e a população enxergar a política como um meio de melhorar a sociedade.",
-                  image: "img/sjc3.jpg",
+                  image: "img/sjc2.jpg",
             }
       ]
 
